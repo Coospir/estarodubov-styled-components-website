5be6804ac83d5cf807523602b37bd72432583abc
Coospir <eugene.starodubov@gmail.com>
2021-02-07 11:47:41 +0300

    added products

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css'
-import {BrowserRouter as Router} from "react-router-dom"
-import {GlobalStyle} from "./globalStyles";
+import { BrowserRouter as Router } from "react-router-dom"
+import { GlobalStyle } from "./globalStyles";
 import Hero from "./components/Hero/Hero";
+import { productData } from "./components/Products/data"
+import Products from "./components/Products/Products";
 
 function App() {
   return (
     <Router>
       <GlobalStyle/>
       <Hero/>
+      <Products heading='Choose your favourite' data={productData}/>
     </Router>
   );
 }
